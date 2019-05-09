125858a62efed5b5db8044d2f3f8248b208225fe
刘文凯 <mister_akai@126.com>
2019-05-09 09:57:53 +0800

    Initial commit

--- assets/settings/physics-settings.js
+++ assets/settings/physics-settings.js
@@ -3,7 +3,8 @@ cc.game.on(cc.game.EVENT_ENGINE_INITED, () => {
     physicsManager.enabled = true;
     physicsManager.enabledAccumulator = false;
     cc.PhysicsManager.FIXED_TIME_STEP = 1 / 60;
-    // physicsManager.debugDrawFlags =
+    physicsManager.debugDrawFlags = cc.PhysicsManager.DrawBits.e_jointBit;
+    // physicsManager.debugDrawFlags = 
     // //     // 0;
     // // cc.PhysicsManager.DrawBits.e_aabbBit |
     // cc.PhysicsManager.DrawBits.e_jointBit |

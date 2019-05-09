b767d798f088038dd32ed114105ce519bb177ad0
刘文凯 <mister_akai@126.com>
2019-05-09 10:14:39 +0800

    Rename

--- assets/script/app/components/RenderMask.ts
+++ assets/script/app/components/RenderMask.ts
@@ -11,7 +11,7 @@
 const { ccclass, property } = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
+export default class RenderMask extends cc.Component {
 
     @property(cc.Camera)
     camera: cc.Camera = null;
